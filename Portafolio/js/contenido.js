a121cc3f15d3662cc017a2298007d22aa01c0a91
FelipeTorres98 <39501267+FelipeTorres98@users.noreply.github.com>
2021-01-18 09:00:14 -0600

    Add files via upload

---
+++ Portafolio/js/contenido.js
@@ -0,0 +1,13 @@
+function mostrar() {
+    document.getElementById("sidebar").style.width = "300px";
+    document.getElementById("contenido").style.marginLeft = "300px";
+    document.getElementById("abrir").style.display = "none";
+    document.getElementById("cerrar").style.display = "inline";
+}
+
+function ocultar() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("contenido").style.marginLeft = "0";
+    document.getElementById("abrir").style.display = "inline";
+    document.getElementById("cerrar").style.display = "none";
+}
